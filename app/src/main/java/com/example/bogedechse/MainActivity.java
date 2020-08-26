4d1f499d4b0cfb61384d657ea59a111877315698
Steven-A. Maack <Steven.maack@tsbw.cloud>
2020-08-26 08:31:07 +0200

    + Added: string TAG = MainActivity

--- app/src/main/java/com/example/bogedechse/MainActivity.java
+++ app/src/main/java/com/example/bogedechse/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";       //Console TAG for Debugging - May be used to output stuff into the console.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
