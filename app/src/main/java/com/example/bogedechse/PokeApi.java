d451bd3cabd21fa0cd08b4a5cc1f3870a0189fe3
Steven-A. Maack <Steven.maack@tsbw.cloud>
2020-08-26 08:31:46 +0200

    + Added: PokeApi class

---
+++ app/src/main/java/com/example/bogedechse/PokeApi.java
@@ -0,0 +1,15 @@
+package com.example.bogedechse;
+
+import android.os.AsyncTask;
+
+public class PokeApi extends AsyncTask {
+
+    public PokeApi() {
+
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}//END POKAPI
